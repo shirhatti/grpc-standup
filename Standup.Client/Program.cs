67f83d12c367ccea5c53a15445347e1257a42742
Sourabh Shirhatti <soshir@microsoft.com>
2019-06-04 11:09:43 -0700

    Initial setup

---
+++ Standup.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Standup.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
